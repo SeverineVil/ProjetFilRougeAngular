42cc3ab09ed98cad8918303578ba70055c762b6a
Pierre-Luc <plbodin@naoconcept.com>
2024-03-19 10:47:00 +0100

    yes no component

--- src/app/components/types-de-biere/types-de-biere.component.ts
+++ src/app/components/types-de-biere/types-de-biere.component.ts
@@ -3,11 +3,14 @@ import { TypeDeBiereService } from '../../services/type-de-biere.service';
 import { TypeDeBiere } from '../../entities/type-de-biere';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgbModal, NgbModalModule, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { YesNoComponent } from '../../core/components/yes-no/yes-no.component';
+import { CreateUpdateTypeDeBiereComponent } from '../create-update-type-de-biere/create-update-type-de-biere.component';
 
 @Component({
   selector: 'app-types-de-biere',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, NgbModalModule],
   templateUrl: './types-de-biere.component.html',
   styleUrl: './types-de-biere.component.scss'
 })
@@ -15,7 +18,25 @@ export class TypesDeBiereComponent {
 
   public typesDeBiere: TypeDeBiere[];
 
-  constructor(private typeDeBiereService: TypeDeBiereService) {
+  constructor(private typeDeBiereService: TypeDeBiereService, private ngbModal: NgbModal) {
     this.typesDeBiere = typeDeBiereService.getAllTypesDeBiere();
+  }
+
+  public delete(typeDeBiere: TypeDeBiere): void {
+
+    const modalRef: NgbModalRef = this.ngbModal.open(YesNoComponent, {
+      centered: true,
+      animation: true
+    });
+
+    const yesNoComponent: YesNoComponent = modalRef.componentInstance;
+
+    yesNoComponent.header = 'Suppression du type de bière';
+    yesNoComponent.body = 'Etes-vous sûr de vouloir supprimer ce type de bière ?';
+    yesNoComponent.yesClicked.subscribe(() => {
+      this.typeDeBiereService.deleteTypeDeBiere(typeDeBiere);
+      modalRef.close();
+    });
+    yesNoComponent.noClicked.subscribe(() => modalRef.close());
   }
 }
