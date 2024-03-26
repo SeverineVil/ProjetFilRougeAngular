bdf1ac8c33cd84de22218082533dbb2fa3475b51
BlackMaster44 <lewis.debato@gmail.com>
2024-03-26 16:12:02 +0100

    commit route caisse

--- src/app/pages/menu-equipe/menu-equipe.component.ts
+++ src/app/pages/menu-equipe/menu-equipe.component.ts
@@ -1,14 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {IMenuRoute} from "../navbar/menu-route";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-menu-equipe',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterLink, RouterLinkActive],
   templateUrl: './menu-equipe.component.html',
   styleUrl: './menu-equipe.component.scss'
 })
 export class MenuEquipeComponent {
 
+  public menuRoutes: IMenuRoute[] = [
+    { path: 'accueil clients', libelle: 'ACCUEIL CLIENTS' },
+    { path: 'reservation', libelle: 'RESERVATIONS' },
+    { path: 'service', libelle: 'SERVICE' },
+    { path: 'cuisine', libelle: 'CUISINE' },
+    { path: 'caisse', libelle: 'CAISSE' },
+  ];
+
+
+
+
+
+  constructor() {
+  }
+
+
+  public navigateTo() {
+
+  }
 }
