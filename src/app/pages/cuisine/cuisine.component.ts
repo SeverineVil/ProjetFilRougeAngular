cb681768437fb6eb2e7d700ecef4064a05f641e5
BlackMaster44 <lewis.debato@gmail.com>
2024-03-28 15:30:56 +0100

    bouton changement de status

--- src/app/pages/cuisine/cuisine.component.ts
+++ src/app/pages/cuisine/cuisine.component.ts
@@ -1,27 +1,42 @@
 import {Component, Input} from '@angular/core';
+import {CuisineService} from "../../services/cuisine.service";
+import {Observable, of, Subscription} from "rxjs";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {PlatService} from "../../services/plat.service";
+import {Commande, Commandes} from "../../entities/commande";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-cuisine',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    AsyncPipe,
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './cuisine.component.html',
   styleUrl: './cuisine.component.scss'
 })
 export class CuisineComponent {
 
-  @Input()
-  private platAenvoyer?: string;
+  public commandes$: Observable<Commande[]>;
 
 
+  constructor(private cuisineService: CuisineService, private platService: PlatService) {
+    this.commandes$ = this.platService.getAllCommandes();
 
+  }
 
-  public cestPret(): boolean{
+  public modifStatut(statut: string, id: number,  commande: Commande): void {
+    this.cuisineService.modifierStatutCommande(statut, id).subscribe({
+      next: (commandeModifiee) => {
+        console.log('Commande mise à jour avec succès:', commandeModifiee);
+        commande.etatCommande= "PRETE";
+      }
+    });
 
-
-    return false;
+  }
 }
 
 
-
-
-}
--- src/app/services/cuisine.service.ts
+++ src/app/services/cuisine.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {PlatService} from "./plat.service";
+import {Commande} from "../entities/commande";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuisineService {
 
-  private commande?: string;
 
 
-  constructor(private httpClient: HttpClient) { }
- /* public recupereLaListeDeCommandes(): Observable<boolean>{
-    return this.httpClient.get<boolean>('http://localhost:8080/commandes').pipe(
-      map((commandes: Commande[]) => {
 
-        if (commandes == null) {
-          return [];
-        }
-        return commandes;
-      })
-    );
-  }*/
+  constructor(private httpClient: HttpClient, private platCommandeService : PlatService) {
+    this.platCommandeService.getAllCommandes();
+  }
+
+  public modifierStatutCommande(statut: string, id: number): Observable<Commande> {
+    const url = `http://localhost:8080/commandes/statut/${id}`;
+    let params = new HttpParams().set('newStatut', statut);
+
+    return this.httpClient.put<Commande>(url, {}, { params: params });
+  }
+
 
 
 
